33456d31e30b7751deb22265a354b6dd2fcbddfb
Glitch (general-grievous-bot) <none>
2023-01-17 14:31:51 +0000

    🍨🚝 Checkpoint ./neko_command.js:7845785/10894

--- neko_command.js
+++ neko_command.js
@@ -1,10 +1,42 @@
-const { SlashCommandBuilder } = require('discord.js');
+const Booru = require("booru");
+const { AttachmentBuilder, EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
+
+const config = require("./config.json");
+const neko_log = require("./neko_log.json");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('neko')
 		.setDescription('Replies with a neko'),
 	async execute(interaction) {
-		await interaction.reply('neko');
+    neko(interaction)
 	},
 };
+
+function neko(interaction) {
+    let siteArray = config.sites;
+        let site = siteArray[Math.floor(Math.random() * siteArray.length)];
+        Booru.search(site, ["nekomimi", "rating:safe", "-comic", "-text", "-high_res"], {
+                limit: 1,
+                random: true,
+            })
+            .then((posts) => {
+                var imageUrl = posts[0].fileUrl;
+                console.log("Sending neko: " + imageUrl + " at " + Date());
+
+                const file = new AttachmentBuilder(imageUrl);
+                const imageEmbed = new EmbedBuilder().setImage(imageUrl);
+
+                interaction.send({ embeds: [imageEmbed], files: [file] });
+            })
+            .catch((err) => {
+                if (err.name === "booruError") {
+                    console.log(err.message);
+                } else {
+                    console.log(err);
+                    console.log("retrying...");
+                    neko(interaction);
+                }
+            });
+}
