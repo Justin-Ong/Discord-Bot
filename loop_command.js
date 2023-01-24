76a37ae958d3d0fd580beb3396ff474d8135a441
Glitch (general-grievous-bot) <none>
2023-01-24 16:12:09 +0000

    👠🐮 Checkpoint ./loop_command.js:7845785/8644

--- loop_command.js
+++ loop_command.js
@@ -6,19 +6,36 @@ module.exports = {
     .setName("loop")
     .setDescription("Starts looping of all songs in queue")
     .addStringOption((option) =>
-      option.setName("one").setDescription("Starts looping current song")
+      option
+        .setName("one")
+        .setDescription("Starts looping current song")
+        .setRequired(false)
     )
     .addStringOption((option) =>
-      option.setName("all").setDescription("Starts looping of all songs in queue")
+      option
+        .setName("all")
+        .setDescription("Starts looping of all songs in queue")
+        .setRequired(false)
     )
     .addStringOption((option) =>
-      option.setName("off").setDescription("Stops looping of song(s)")
+      option
+        .setName("off")
+        .setDescription("Stops looping of song(s)")
+        .setRequired(false)
     ),
   async execute(interaction) {
     if (interaction.options.getSubcommand() === "one") {
+      await interaction.reply("Now looping current song");
+      pc.isLoopingOne = true;
+      pc.isLoopingAll = false;
+    } else if (interaction.options.getSubcommand() === "all") {
       await interaction.reply("Now looping all songs");
       pc.isLoopingOne = false;
       pc.isLoopingAll = true;
+    } else if (interaction.options.getSubcommand() === "off") {
+      await interaction.reply("Stopping looping of song(s)");
+      pc.isLoopingOne = false;
+      pc.isLoopingAll = false;
     }
   },
 };
