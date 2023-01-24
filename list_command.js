2b345f5472c34cc00adc7d54471a14bccf32003b
Glitch (general-grievous-bot) <none>
2023-01-24 08:35:59 +0000

    🎲🏂 Checkpoint ./list_command.js:7845785/11246

--- list_command.js
+++ list_command.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { getVoiceConnection } = require("@discordjs/voice");
+const ytdl = require("ytdl-core");
 
 const pc = require("./play_command");
 
@@ -8,11 +9,41 @@ module.exports = {
     .setName("list")
     .setDescription("Lists current playlist"),
   async execute(interaction) {
-    await interaction.reply("Skipping current song");
-    let channel = interaction.member.voice.channel;
-    if (pc.playlist.length >= 1) {
-      pc.playlist.shift();
-      pc.playSong();
-    }
+    await interaction.deferReply();
+    await getList(interaction);
   },
 };
+
+async function getList(interaction) {
+  if (!pc.playlist.length) {
+    interaction.editReply("there are no songs in the queue!");
+  } else {
+    try {
+      let result = "";
+      for (let i = 0; i < 5; i++) {
+        let song = pc.playlist[i];
+        if (song === undefined) {
+          break;
+        }
+        let info = await ytdl.getInfo(song);
+        let song_title = info.videoDetails.title;
+        let song_duration = new Date(info.videoDetails.lengthSeconds * 1000)
+          .toISOString()
+          .substr(11, 8);
+        result +=
+          "Song " +
+          (i + 1) +
+          ": " +
+          song_title +
+          ", Duration: " +
+          song_duration +
+          "\n";
+      }
+      result += pc.playlist.length + " songs in queue";
+      interaction.editReply(result);
+    } catch (err) {
+      console.log(err);
+      interaction.editReply("Sorry, an error occurred.");
+    }
+  }
+}
